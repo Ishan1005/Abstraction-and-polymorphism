1308920e0dc6f145f439877df26a3dc42363d342
Ishan1005 <ig20april@gmail.com>
2024-11-23 12:03:36 +0400

    Add files via upload

---
+++ Ferrari And BMW/Main.py
@@ -0,0 +1,12 @@
+class BMW:
+    def capital(self):
+        print("350 is the max speed of a BMW.")
+    
+class Ferrari:
+    def capital1(self):
+        print("375 is the max speed of a Ferrari.")
+    
+obj = BMW()
+obj.capital()
+obj1 = Ferrari()
+obj1.capital1()
